2fb4783b354108d12121852a388d5e5142d04f7b
dolke <dolkeshubhankar@gmail.com>
2024-01-20 23:41:36 +0530

    Test Commit

---
+++ src/ArrayListPackage/ArrayListPractice.java
@@ -0,0 +1,11 @@
+package ArrayListPackage;
+
+import java.util.ArrayList;
+
+public class ArrayListPractice {
+
+    public static void manin(String [] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+
+    }
+}
