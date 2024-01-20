a5132d27d643fa9ab029134660d333095facc15b
dolke <dolkeshubhankar@gmail.com>
2024-01-20 23:44:58 +0530

    Test Commit

--- src/ArrayListPackage/ArrayListPractice.java
+++ src/ArrayListPackage/ArrayListPractice.java
@@ -4,8 +4,13 @@ import java.util.ArrayList;
 
 public class ArrayListPractice {
 
-    public static void manin(String [] args) {
+    public static void main(String [] args) {
         ArrayList<Integer> arr = new ArrayList<>();
+
+        arr.add(5);
+        arr.add(44);
+        arr.add(1,4);
+        System.out.println(arr.get(1));
 
     }
 }
