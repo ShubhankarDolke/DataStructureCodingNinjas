1e98d0856439349ccccd68dad2cb773ebc39806a
dolke <dolkeshubhankar@gmail.com>
2024-01-22 03:44:25 +0530

    Basic Implementation of LL Node, data and Next ->manual print ->loop printing -> use temp for head pass by value

--- src/TestFileHelloWorld.java
+++ src/TestFileHelloWorld.java
@@ -1,6 +1,6 @@
 public class TestFileHelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out. println(1 + 1 + 1 + 1 + 1 == 5);
     }
 }
