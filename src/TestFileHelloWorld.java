c89453e75fac1c01719fe3bc4db4a925d83ef342
dolke <dolkeshubhankar@gmail.com>
2024-04-28 20:27:29 +0530

    commit final

--- src/TestFileHelloWorld.java
+++ src/TestFileHelloWorld.java
@@ -4,12 +4,7 @@ import java.util.Queue;
 import java.util.Stack;
 class TestFileHelloWorld {
     public static void main (String[] args) {
-        Queue<Integer> q=new LinkedList<Integer>();
-        Stack<Integer> s=new Stack<Integer>();
-        q.add(5);
-        q.add(10);
-        s.push(10);
-        s.push(20);
-        System.out.print(q.peek()+s.pop());
+
+
     }
 }
